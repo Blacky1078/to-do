46c4b3e35e399e1106e0072b3440624bfa63d454
Shivansh Goel <92398906+Blacky1078@users.noreply.github.com>
2024-03-17 12:13:35 +0530

    Wip

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,8 +1,4 @@
-import {
-  HttpClient,
-  HttpErrorResponse,
-  HttpHeaders,
-} from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import 'firebase/compat/firestore'; //
 import 'firebase/firestore';
@@ -18,11 +14,7 @@ import { MessageService } from 'primeng/api';
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(
-    private firestore: AngularFirestore,
-    private http: HttpClient,
-    private message: MessageService
-  ) {}
+  constructor(private firestore: AngularFirestore, private http: HttpClient,private message: MessageService) {}
 
   getUser(User_email: any): Observable<any> {
     const url = 'http://localhost:3000/user';
@@ -31,9 +23,7 @@ export class AuthService {
         'Content-Type': 'application/json',
       }),
     };
-    return this.http
-      .post<any>(url, User_email, httpOptions)
-      .pipe(catchError(this.handleError));
+    return this.http.post<any>(url, User_email, httpOptions).pipe(catchError(this.handleError))
   }
 
   createUser(userData: any): Observable<any> {
@@ -43,9 +33,7 @@ export class AuthService {
         'Content-Type': 'application/json',
       }),
     };
-    return this.http
-      .post<any>(url, userData, httpOptions)
-      .pipe(catchError(this.handleError));
+    return this.http.post<any>(url, userData, httpOptions).pipe(catchError(this.handleError))
   }
 
   createTODO(userTodo: any): Observable<any> {
@@ -55,9 +43,7 @@ export class AuthService {
         'Content-Type': 'application/json',
       }),
     };
-    return this.http
-      .post<any>(url, userTodo, httpOptions)
-      .pipe(catchError(this.handleError));
+    return this.http.post<any>(url, userTodo, httpOptions).pipe(catchError(this.handleError))
   }
 
   getTODO(User_email: any) {
@@ -67,9 +53,7 @@ export class AuthService {
         'Content-Type': 'application/json',
       }),
     };
-    return this.http
-      .post<any>(url, User_email, httpOptions)
-      .pipe(catchError(this.handleError));
+    return this.http.post<any>(url, User_email, httpOptions).pipe(catchError(this.handleError))
   }
   editTODO(User_todo: any) {
     const url = 'http://localhost:3000/EditUserTodo';
@@ -78,22 +62,21 @@ export class AuthService {
         'Content-Type': 'application/json',
       }),
     };
-    return this.http
-      .post<any>(url, User_todo, httpOptions)
-      .pipe(catchError(this.handleError));
+    return this.http.post<any>(url, User_todo, httpOptions).pipe(catchError(this.handleError))
   }
 
-  private handleError(error: HttpErrorResponse) {  //Net Generated
+  private handleError(error: HttpErrorResponse) {
     let errorMessage = 'Unknown error occurred';
     if (error.error instanceof ErrorEvent) {
+     
       errorMessage = `Error: ${error.error.message}`;
     } else {
+   
       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
-
-    alert('Backend Server Error ---->  ' + errorMessage);
+    
+    alert("Backend Server Error ---->  " + errorMessage)
     return throwError(errorMessage);
   }
-
-//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
+ 
 }
